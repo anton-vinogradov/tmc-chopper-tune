54774b79b19c06f4383a3871bd228ed8ab036155
Anton Vinogradov <av@apache.org>
2024-07-14 19:42:10 +0300

    WIP

--- chopper_plot.py
+++ chopper_plot.py
@@ -172,7 +172,7 @@ def main():
 
 def message(msg):
     def task():
-        os.system("curl http://127.0.0.1:7125/printer/gcode/script?script=%s" % msg)
+        os.system("curl -s http://127.0.0.1:7125/printer/gcode/script?script=%s" % msg)
 
     thread = Thread(target=task)
     thread.start()
