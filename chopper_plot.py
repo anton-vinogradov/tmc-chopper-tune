54daf9efc15615b3ef9ded294c798cd167b26e60
Anton Vinogradov <av@apache.org>
2024-07-14 19:55:30 +0300

    WIP

--- chopper_plot.py
+++ chopper_plot.py
@@ -172,12 +172,7 @@ def main():
 
 
 def message(msg):
-    def task():
-        printer.send_gcode("M118 %s") % msg
-
-    thread = Thread(target=task)
-    thread.start()
-    thread.join()
+    printer.send_gcode("M118 %s") % msg
 
 
 if __name__ == '__main__':
