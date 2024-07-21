92dcfb97be726914907c1248aec4a76ac755b8d9
Anton Vinogradov <av@apache.org>
2024-07-21 20:54:16 +0300

    WIP

--- tuner.sh
+++ tuner.sh
@@ -2,4 +2,4 @@
 script_path=$(realpath $(echo $0))
 repo_path=$(dirname ${script_path})
 source ${repo_path}/.venv/bin/activate
-exec python3 ${repo_path}/tune.py "$@"
+exec python3 ${repo_path}/tuner.py "$@"
